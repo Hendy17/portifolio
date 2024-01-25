483e80d16cd5a367026c0d7a9e090935d380d6cf
Hendy17 <hendyvorpagel@gmail.com>
2024-01-24 21:19:10 -0300

    Atul.mob

--- scripts.js
+++ scripts.js
@@ -1,9 +1,17 @@
-let header = document.getElementById('header');
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY >= 200) {
-    header.style.background = '#191919';
-  } else {
-    header.style.background = 'transparent';
-  }
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  paginatio: {
+    el: ".swiper-paginatio",
+    clickable: true,
+  },
+  navigatio: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
+
+let header = document.getElementById('header')
+
+window.addEventListener('scroll', () => {  
+})
