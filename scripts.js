e6b6130ac2755ce5a46d02884f1e50a36a8c88eb
Hendy17 <hendyvorpagel@gmail.com>
2024-01-25 21:34:39 -0300

    Atualização Projetos

--- scripts.js
+++ scripts.js
@@ -1,17 +1,21 @@
-let swiper = new Swiper(".mySwiper", {
+let swiper = new Swiper(".swiper-mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
-  paginatio: {
-    el: ".swiper-paginatio",
+  pagination: {
+    el: ".swiper-pagination",
     clickable: true,
   },
-  navigatio: {
+  navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
 });
 
 let header = document.getElementById('header')
-
-window.addEventListener('scroll', () => {  
-})
+window.addEventListener('scroll', () => { 
+  if (window.scrollY >= 200) { 
+    header.classList.add('scrolled'); 
+  } else {
+    header.classList.remove('scrolled'); 
+  }
+});
