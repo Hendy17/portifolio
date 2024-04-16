13fc0e30eae0a187074f694379c7409cec18afb1
Hendy17 <hendyvorpagel@gmail.com>
2024-04-16 16:01:01 -0300

    Atualização

--- scripts.js
+++ scripts.js
@@ -1,21 +1,21 @@
-let swiper = new Swiper(".swiper-mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});;
+document.addEventListener('DOMContentLoaded', function() {
+  // Inicialização do Swiper
+  var swiper = new Swiper('.swiper-mySwiper', {
+    slidesPerView: 1, // Apenas um slide visível de cada vez
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 
-let header = document.getElementById('header')
-window.addEventListener('scroll', () => { 
-  if (window.scrollY >= 200) { 
-    header.classList.add('scrolled'); 
-  } else {
-    header.classList.remove('scrolled'); 
-  }
+  // Outras funções JavaScript, como o código para alterar a classe do header com base no scroll
+  let header = document.getElementById('header');
+  window.addEventListener('scroll', () => { 
+    if (window.scrollY >= 200) { 
+      header.classList.add('scrolled'); 
+    } else {
+      header.classList.remove('scrolled'); 
+    }
+  });
 });
