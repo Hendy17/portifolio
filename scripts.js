575dc7ab71efa7accd9eb163d16c6097d7a2292f
Hendy17 <hendyvorpagel@gmail.com>
2024-06-25 09:03:43 -0300

    Ataualização

--- scripts.js
+++ scripts.js
@@ -1,21 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Inicialização do Swiper
-  var swiper = new Swiper('.swiper-mySwiper', {
-    slidesPerView: 1, // Apenas um slide visível de cada vez
-    spaceBetween: 30,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+// Inicialização do Swiper
+var swiper = new Swiper('.swiper-mySwiper', {
+  slidesPerView: 1, // Define quantos slides serão exibidos simultaneamente
+  spaceBetween: 30, // Define o espaçamento entre os slides
+  navigation: {
+    nextEl: '.swiper-button-next', // Seletor para o botão "Próximo"
+    prevEl: '.swiper-button-prev', // Seletor para o botão "Anterior"
+  },
+});
 
-  // Outras funções JavaScript, como o código para alterar a classe do header com base no scroll
-  let header = document.getElementById('header');
-  window.addEventListener('scroll', () => { 
-    if (window.scrollY >= 200) { 
-      header.classList.add('scrolled'); 
-    } else {
-      header.classList.remove('scrolled'); 
-    }
-  });
+// Adiciona um ouvinte de evento de rolagem à janela
+window.addEventListener('scroll', function() {
+  var header = document.getElementById('header');
+  // Verifica a posição de rolagem da página
+  if (window.scrollY >= 200) {
+    // Adiciona a classe 'scrolled' ao elemento com o ID 'header'
+    header.classList.add('scrolled');
+  } else {
+    // Remove a classe 'scrolled' do elemento com o ID 'header'
+    header.classList.remove('scrolled');
+  }
 });
