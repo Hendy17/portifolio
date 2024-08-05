3e86540e7a9e1cf5abc2ca16fca992f0a4d16abe
Hendy17 <hendyvorpagel@gmail.com>
2024-08-05 20:31:28 -0300

    Atualização

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
 // Inicialização do Swiper
 var swiper = new Swiper('.swiper-mySwiper', {
-  slidesPerView: 1,
-  spaceBetween: 30,
+  slidesPerView: 1, // Define quantos slides serão exibidos simultaneamente
+  spaceBetween: 30, // Define o espaçamento entre os slides
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next', // Seletor para o botão "Próximo"
+    prevEl: '.swiper-button-prev', // Seletor para o botão "Anterior"
   },
+});
 
 // Adiciona um ouvinte de evento de rolagem à janela
 window.addEventListener('scroll', function() {
